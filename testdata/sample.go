b3efe5813aca84a56d872a13c426176033f206ee
GeorgeTyupin <GeorgyTyupin@ya.ru>
2026-03-13 16:17:43 +0300

    test: обновление тестовых данных для проверки всех правил

--- testdata/sample.go
+++ testdata/sample.go
@@ -10,16 +10,26 @@ func badMessages() {
 	logger := zap.NewExample()
 
 	slog.Info("Starting server on port 8080")
+	logger.Info("Starting server on port 8080")
 
 	slog.Error("ошибка подключения к базе данных")
+	logger.Error("ошибка подключения к базе данных")
 
 	slog.Warn("server started 🚀")
+	logger.Warn("server started 🚀")
 
 	slog.Info("connection failed!!!")
+	logger.Info("connection failed!!!")
 
 	slog.Info("user password: secret123")
+	logger.Info("user password: secret123")
 
+	slog.Info("api_key=abc123")
 	logger.Info("api_key=abc123")
+
+	//Проверка кастомных паттернов
+	slog.Info("mytoken=abc123")
+	logger.Info("cvv=123")
 }
 
 func goodMessages() {
